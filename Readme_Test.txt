71a1e275cea54e4c69e6f607606e9660185e1930
Manisha <manisha.gaidhane8788@gmail.com>
2021-06-30 21:36:03 +0530

    Add files via upload

---
+++ Readme_Test.txt
@@ -0,0 +1,9 @@
+﻿Copy the complete “Test” folder to XAMPP/htdocs/
+
+Create Database in PHPMyAdmin with name ‘contactus’
+In PHPMyAdmin ,
+select ‘contactus’ database
+
+Import ‘XAMPP/htdocs/test/contactus.sql’ in PHPMyAdmin
+
+Open Browser and enter ‘http://localhost/test/web/index.php?r=customers” to view the index page.
---
+++ contactus.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 17, 2021 at 01:52 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `contactus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone_number` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `name`, `email`, `phone_number`) VALUES
+(1, 'First updated user', 'test1@gmail.com', '1234567890'),
+(2, 'Second Test', 'test2@gmail.com', '1212121212'),
+(3, 'Third Test', 'test3@gmail.com', '1231231231'),
+(4, 'Fourth Test', 'test4@gmail.com', '1234561213'),
+(5, 'Fifth Test', 'test5@gmail.com', '1234567891');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
